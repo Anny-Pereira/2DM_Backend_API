433f02197647a37de6e1898718eb784b17aa7a9e
Anny Pereira <isabelle.pereira04@gmail.com>
2021-08-26 11:32:55 -0300

    Exemplo - Repositório e Controller

---
+++ Exemplos/Senai_Filmes_WebAPI/Senai_Filmes_WebAPI/Controllers/GeneroController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai_Filmes_WebAPI.Domains;
+using Senai_Filmes_WebAPI.Interfaces;
+using Senai_Filmes_WebAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// Controlador responsável pelo end points referentes ao genero
+/// </summary>
+
+namespace Senai_Filmes_WebAPI.Controllers
+{
+
+    //Define que o tipo de resposta da API será no formato JSON
+    [Produces("application/json")]
+
+
+    //Define que a rota de uma requisição será no formato domínio/api/nomeController
+    //Ex: http://localhost:5000/api/generos
+    [Route("api/[controller]")]
+
+    //Define que é um controlador de API
+    [ApiController]
+    public class GeneroController : ControllerBase
+    {
+
+        /// <summary>
+        /// Objeto que irá receber todos os métodos definidos na interface
+        /// </summary>
+        private IGeneroRepository _GeneroRepository { get; set; }
+
+
+        /// <summary>
+        /// Instancia um objeto _GeneroRepository para que haja referencia do métodos no repositório
+        /// </summary>
+        public GeneroController()
+        {
+            _GeneroRepository = new GeneroRepository();
+        }
+
+        [HttpGet]
+        //IActionResult = Resultado de uma ação
+        //Get() = nome genérico
+        public IActionResult Get()
+        {
+            //Devolve Lista de generos
+            //Se conecta com o repositório
+
+
+            //Cria uma lista nomeada listaGeneros para receber os dados
+            List<GeneroDomain> listaGeneros = _GeneroRepository.ListarTodos();
+
+
+            //Retorna os status code 200(OK) com a lista generos no formato JSON
+            return Ok(listaGeneros);
+
+        }
+
+    }
+}
---
+++ Exemplos/Senai_Filmes_WebAPI/Senai_Filmes_WebAPI/Repositories/GeneroRepository.cs
@@ -0,0 +1,102 @@
+﻿using Senai_Filmes_WebAPI.Domains;
+using Senai_Filmes_WebAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_Filmes_WebAPI.Repositories
+{
+    public class GeneroRepository : IGeneroRepository
+    {
+        //Trocar o nome de servidor quando mudar de máquina
+
+        /// <summary>
+        /// String de conexãocom o banco de dados que recebe os parâmetros
+        /// Data Source = Nome do Servidor
+        /// initial catalog = nome do bando de dados
+        /// integrated security= faz a autenticação com o usuário do windows
+        /// </summary>
+        private string stringConexao = "Data Source =DESKTOP-TUQ4VJR\\SQLEXPRESS; initial catalog= CATALOGO; user id=sa; pwd=senai@132";
+
+        //
+
+        public void AtualizarIdUrl(int idGenero, GeneroDomain genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AtulizarIdCorpo(GeneroDomain genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GeneroDomain BuscarId(int idGenero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(GeneroDomain NovoGenero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int IdGenero)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary>
+        /// Lista todos os gêneros
+        /// </summary>
+        /// <returns>listaGenero</returns>
+        public List<GeneroDomain> ListarTodos()
+        {
+            List<GeneroDomain> listaGenero = new List<GeneroDomain>();
+
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                //Declara instruçlão a ser executada
+                string querySelectALL = "SELECT idGenero, nomeGenero FROM Genero";
+
+
+                //Abre a conexão com o banco de dados
+                con.Open();
+
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(querySelectALL, con))
+                {
+                    //Executa a query
+                    rdr = cmd.ExecuteReader();
+
+
+                    //Enquanto houver registros para serem lidos no rdr, o laço se repete.
+                    while (rdr.Read())
+                    {
+                        //Instancia de um objeto genero do tipo GeneroDomain
+                        GeneroDomain genero = new GeneroDomain()
+                        {
+
+                            //Atribui a propriedade idGenero o valor da primeira coluna na tabela do banco de dados
+                            idGenero = Convert.ToInt32(rdr[0]),
+
+                            //atribui a propriedade nomeGenero o valor da segunda coluna na tabela do banco de dados
+                            nomeGenero = rdr[1].ToString()
+                        };
+
+
+                        //Adiciona o objeto genero criado à lista listaGenero
+                        listaGenero.Add(genero);
+                    }
+
+                   
+                }
+            }
+
+            return listaGenero;
+        }
+    }
+}
